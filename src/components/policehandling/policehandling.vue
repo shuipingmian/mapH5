37154fb2a496fbb36dcad628786e6880d0f823ff
wumingzi <975365122@qq.com>
2020-04-03 13:37:12 +0800

    hebing

--- src/components/policehandling/policehandling.vue
+++ src/components/policehandling/policehandling.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="page">
-    policeheanding11
+    <div id="mapContainer"></div>
   </div>
 </template>
 
@@ -12,14 +12,41 @@ export default {
   data() {
     return {
     }
+  },
+  mounted() {
+    this.mapInit()
+  },
+  methods: {
+    mapInit() {
+      var map = new AMap.Map("mapContainer", { // eslint-disable-line no-undef
+        resizeEnable: true,
+        zoom: 11, // 级别
+        center: [116.397428, 39.90923]// 中心点坐标
+      });
+      var marker = new AMap.Marker({ // eslint-disable-line no-undef
+        position: new AMap.LngLat(116.39, 39.9), // eslint-disable-line no-undef
+        title: "北京"
+      });
+      map.add(marker)
+    }
   }
+
 }
 </script>
 
-<style scoped lang="less">
+<style scoped>
 .page {
   background-color: red;
   width: 100%;
   height: 100%;
+}
+#mapContainer {
+  height: 94%;
+}
+.amap-logo {
+  display: none !important;
+}
+.amap-copyright {
+  opacity: 0 !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import './cube-ui'
-import App from './App.vue'
-import router from './router'
-import 'amfe-flexible'
+import Vue from "vue"
+import "./cube-ui"
+import App from "./App.vue"
+import router from "./router"
+import "amfe-flexible"
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app")
