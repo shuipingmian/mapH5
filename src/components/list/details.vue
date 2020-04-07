f6bcf9477fd1d1f884fa5f802a613e0502c32b64
yiyiren <15350014683@163.com>
2020-04-07 17:57:15 +0800

    合并分之

--- src/components/list/details.vue
+++ src/components/list/details.vue
@@ -1,34 +1,28 @@
 <template>
-
   <cube-popup id="list" class="popup" position="bottom" :mask="false">
     <div class="gongdan">
       <ul class="gongdanList">
-        <cube-scroll
-          ref="scroll"
-          :data="virtualData"
-          class="horizontal-scroll-list-wrap"
-        >
-          <li v-for="item in virtualData" :key="item.id" style=" display: inline-block">
-            <div class="information">
-              <div class="title">
-                <div :class="item.color"></div>
-                <p>{{ item.title }}</p>
-              </div>
-              <div class="content">
-                <p>{{ item.content }}</p>
-              </div>
-              <div class="timing">
-                <p>{{ item.timing }}</p>
-              </div>
-              <div class="source">
-                <p>{{ item.source }}</p>
-              </div>
+        <li v-for="item in virtualData" :key="item.id">
+          <div class="information">
+            <div class="title">
+              <div :class="item.color"></div>
+              <p>{{ item.title }}</p>
             </div>
-            <div class="btn">
-              <p class="result">未处理</p>
+            <div class="content">
+              <p>{{ item.content }}</p>
             </div>
-          </li>
-        </cube-scroll></ul>
+            <div class="timing">
+              <p>{{ item.timing }}</p>
+            </div>
+            <div class="source">
+              <p>{{ item.source }}</p>
+            </div>
+          </div>
+          <div class="btn">
+            <p class="result">未处理</p>
+          </div>
+        </li>
+      </ul>
       <!-- <div class="details">
         <div class="title">
           <p>事故车辆信息</p>
@@ -46,9 +40,7 @@
       <div class="videoList">
 
       </div> -->
-
     </div>
-
   </cube-popup>
 </template>
 <script>
@@ -57,10 +49,6 @@ export default {
   props: ["detailData"],
   data() {
     return {
-      scrollOptions: {
-        click: false,
-        directionLockThreshold: 0
-      },
       virtualData: [
         { id: 1, title: "交通执法", content: "江南大道江虹路北汽车刮蹭事故", timing: "2020/03/21 00:00:18", source: "来源：警务系统", color: "ball-red ball" }
       ]
@@ -87,28 +75,15 @@ export default {
   position: absolute;
   overflow: hidden;
   width: 100%;
-
 }
-
 .gongdan{
   padding-bottom:60px;
   background-color: #fff;
   border-top-left-radius: 12px;
   border-top-right-radius: 12px;
-  #ratings{
-    position: relative;
-    text-align: left;
-    white-space: normal;
-    height: 100%
-}
- .horizontal-scroll-list-wrap{
-     .cube-scroll-content{
-         display: inline-block
-         }
   .gongdanList {
-    // overflow-y: auto;
-     white-space: nowrap;
-     height:220px;
+    overflow-y: auto;
+    max-height:220px;
     li{
       display: flex;
       justify-content: space-between;
@@ -169,7 +144,6 @@ export default {
         }
       }
     }
-    }
   }
   .details{
     width:100%;
@@ -177,6 +151,5 @@ export default {
       font-size:13.33px;
     }
   }
-
 }
 </style>
