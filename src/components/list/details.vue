473524880c8dcb464bc21060b79d1144348e098e
yiyiren <15350014683@163.com>
2020-04-08 16:37:59 +0800

    打印日志修改

--- src/components/list/details.vue
+++ src/components/list/details.vue
@@ -79,7 +79,6 @@ export default {
   },
   methods: {
     checkheight() {
-      console.log(this.$refs.comheight.offsetHeight)
       if (this.$refs.comheight.offsetHeight >= 300) {
         this.iconcheck = false
         this.$refs.comheight.style.height = "260px";
